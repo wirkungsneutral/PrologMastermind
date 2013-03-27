847fd4c25b66bd86915ec11841e13bd0c83ce01d
Matthis Hauschild <matthis.hauschild@gmail.com>
2013-03-28 00:29:33 +0100

    stuff

--- mastermind.pl
+++ mastermind.pl
@@ -1,3 +1,5 @@
+:- use_module(library(random)).
+:- use_module(library(clpfd)).
 
 %http://www.cs.oswego.edu/~odendahl/coursework/notes/prolog/synopsis/con.html
 a([]) :- give_random_color(C),print_color(C).
@@ -6,7 +8,10 @@ println(A):-write(A),nl.
 %print(A):-write(A).
 %test([]) : give_white([1,3,3,1],[1, 2, 2 ,2],).
 
-give_random_color(C):- random_between(1,6,C).
+%Fuer Windows
+%give_random_color(C):- random_between(1,6,C).
+%Fuer Linux, obwohl deprecated, aber random_between findet er nicht, wohl zu neu
+give_random_color(C):- random(1,7,C).
 
 print_color(1):- write('green').
 print_color(2):- write('yellow').
--- testcases.pl
+++ testcases.pl
@@ -1,6 +1,6 @@
 consult('mastermind.pl').
 
-get_member_pos(2, [1,3,2,4], 3).
+%get_member_pos(2, [1,3,2,4], 3).
 
 println('calc_guesses').
 calc_guess([1,2,3,4], [1,2,3,4], 4, 0).
@@ -9,8 +9,9 @@ calc_guess([1,1,1,1], [1,2,3,4], 1, 0).
 calc_guess([1,2,3,4], [4,3,2,1], 0, 4).
 calc_guess([1,2,1,2], [1,2,3,4], 2, 0).
 
-println('delete first occurence').
-delete_first_occurence([1,2,3,4], 2, [1,3,4]).
-delete_first_occurence([1,2,2,4], 2, [1,2,4]).
-delete_first_occurence([1,2,3,4], 5, [1,2,3,4]).
-delete_first_occurence([], 2, []).
+%funzen noch net, kA wieso
+%println('delete first occurence').
+%delete_first_occurence([1,2,3,4], 2, [1,3,4]).
+%delete_first_occurence([1,2,2,4], 2, [1,2,4]).
+%delete_first_occurence([1,2,3,4], 5, [1,2,3,4]).
+%delete_first_occurence([], 2, []).
